40e00ad0367bcf3f49756e29acbbdae84d5e2c28
Balwidner Katoch <vickykatoch@yahoo.com>
2016-05-15 11:42:55 -0400

    initial

---
+++ Acme.Domain/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.Domain
+{
+    public class Class1
+    {
+    }
+}
